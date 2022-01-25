953f01f02287754641c3e5c14f8eac07ac3019c0
iKurum <cyan@ikurum.cn>
2022-01-25 15:34:20 +0800

    get short

--- v1/short/get.go
+++ v1/short/get.go
@@ -1,9 +1,11 @@
 package v1
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"api.ikurum.cn/global"
 	"api.ikurum.cn/route"
@@ -54,7 +56,20 @@ func init() {
 		if sr == "" {
 			rw.Write(msg)
 		} else {
-			http.Redirect(rw, r, sr, http.StatusFound)
+			if strings.HasPrefix(sr, "https://") || strings.HasPrefix(sr, "http://") {
+				http.Redirect(rw, r, sr, http.StatusFound)
+			} else {
+				sDec, err := base64.StdEncoding.DecodeString(sr)
+				if err != nil {
+					msg, _ = json.Marshal(global.NewResult(&global.Result{
+						Code: 0,
+						Msg:  err,
+					}))
+					rw.Write(msg)
+				} else {
+					http.Redirect(rw, r, string(sDec), http.StatusFound)
+				}
+			}
 		}
 	})
 }
